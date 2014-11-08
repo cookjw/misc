5df13010c87518b38e7b0c016c19e6736c653d69
James Cook <cookjw@gmail.com>
2014-11-07 22:38:50 -0800

    terminal velocity; now works as envisioned

--- fallsim.py
+++ fallsim.py
@@ -1,41 +1,38 @@
-# credit: http://stackoverflow.com/a/3249537
+from time import time
 
+acceleration = 32.0
+terminal_velocity = 183.7
+time_to_terminal_velocity = terminal_velocity/acceleration
+distance_to_terminal_velocity = 0.5*acceleration*(time_to_terminal_velocity)**2
+def distance_fallen(time):
+    if time <= time_to_terminal_velocity:
+        distance = 0.5*acceleration*(time**2)
+    else:
+         
+        distance = distance_to_terminal_velocity + terminal_velocity*(
+            time - time_to_terminal_velocity
+            )   
+    return distance    
 
-from sys import stdout
-from time import time, sleep
-
-starting_altitude = int(raw_input("Enter starting altitude.\n"))
-digits = len(str(starting_altitude))
-delete = "\b" * (digits + 1)
-acceleration = int(raw_input("Enter acceleration. \n"))
-# inverse_speed = 1.0/speed # http://lesswrong.com/lw/4z2/inverse_speed/
-digits = len(str(starting_altitude))
-delete = "\b" * (digits + 1)
+starting_altitude = int(raw_input("Enter starting altitude in feet.\n"))
 current_altitude = starting_altitude
 start_time = time()
-# while current_altitude > 0:
-sleep(0.05)
-for i in range(starting_altitude + 1):
+while current_altitude > 0:
     elapsed_time = time() - start_time
-    # print i, elapsed_time
-    speed = - acceleration * elapsed_time
-    # print "speed: " + str(speed)
-    inverse_speed = -1.0/speed # http://lesswrong.com/lw/4z2/inverse_speed/
-    sleep(inverse_speed)
-    print "{0}{1:{2}}".format(delete, starting_altitude - i, digits),
+    if elapsed_time <= time_to_terminal_velocity:
+        speed = acceleration*elapsed_time
+    else:
+        speed = terminal_velocity
+    current_altitude = starting_altitude - distance_fallen(elapsed_time) 
+    if current_altitude >= 0:    
+        print "\r", "altitude: ", round(current_altitude, 1), "ft ", \
+            "speed:", round(speed, 1), "ft/sec ", \
+            "time:", round(elapsed_time, 1), "sec", 
     
-    
-print "\n IMPACT " + str(elapsed_time)  + " sec."
-    
+print "\n 0 \n IMPACT " + str(round(elapsed_time, 1))  + " sec" + \
+    "\n speed: {0} feet/sec".format(round(speed, 1))
 
-
-# for i in range(starting_altitude + 1):
-    # if speed != 0:
-        # inverse_speed = 1.0/speed    
     
-    # sleep(inverse_speed)    
-    # print "{0}{1:{2}}".format(delete, starting_altitude - i, digits),
-# print "\n IMPACT"
 
 
 
