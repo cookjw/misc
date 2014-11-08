9dd52fff6962422627c57000a88b2c1451c16700
James Cook <cookjw@gmail.com>
2014-11-07 22:50:00 -0800

    style

--- fallsim.py
+++ fallsim.py
@@ -3,12 +3,13 @@ from time import time
 acceleration = 32.0
 terminal_velocity = 183.7
 time_to_terminal_velocity = terminal_velocity/acceleration
-distance_to_terminal_velocity = 0.5*acceleration*(time_to_terminal_velocity)**2
+distance_to_terminal_velocity = 0.5*acceleration*(
+    time_to_terminal_velocity
+    )**2
 def distance_fallen(time):
     if time <= time_to_terminal_velocity:
         distance = 0.5*acceleration*(time**2)
-    else:
-         
+    else:         
         distance = distance_to_terminal_velocity + terminal_velocity*(
             time - time_to_terminal_velocity
             )   
