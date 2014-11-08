f3cbf755f3222b6e116f9ca6935033f1c27c32a6
James Cook <cookjw@gmail.com>
2014-11-07 19:47:04 -0800

    acceleration

--- fallsim.py
+++ fallsim.py
@@ -1,12 +1,41 @@
+# credit: http://stackoverflow.com/a/3249537
+
+
 from sys import stdout
-from time import sleep
+from time import time, sleep
 
 starting_altitude = int(raw_input("Enter starting altitude.\n"))
-# credit: http://stackoverflow.com/a/3249537
 digits = len(str(starting_altitude))
 delete = "\b" * (digits + 1)
+acceleration = int(raw_input("Enter acceleration. \n"))
+# inverse_speed = 1.0/speed # http://lesswrong.com/lw/4z2/inverse_speed/
+digits = len(str(starting_altitude))
+delete = "\b" * (digits + 1)
+current_altitude = starting_altitude
+start_time = time()
+# while current_altitude > 0:
+sleep(0.05)
 for i in range(starting_altitude + 1):
-    sleep(0.5)    
+    elapsed_time = time() - start_time
+    # print i, elapsed_time
+    speed = - acceleration * elapsed_time
+    # print "speed: " + str(speed)
+    inverse_speed = -1.0/speed # http://lesswrong.com/lw/4z2/inverse_speed/
+    sleep(inverse_speed)
     print "{0}{1:{2}}".format(delete, starting_altitude - i, digits),
-print "\n IMPACT"
+    
+    
+print "\n IMPACT " + str(elapsed_time)  + " sec."
+    
+
+
+# for i in range(starting_altitude + 1):
+    # if speed != 0:
+        # inverse_speed = 1.0/speed    
+    
+    # sleep(inverse_speed)    
+    # print "{0}{1:{2}}".format(delete, starting_altitude - i, digits),
+# print "\n IMPACT"
+
+
 
