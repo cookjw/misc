1421cc2d69d1ac8c74f86973bba85a42d5f54e60
James Cook <cookjw@gmail.com>
2014-11-10 20:53:05 -0800

    minor style edit

--- selectionsort.rb
+++ selectionsort.rb
@@ -19,7 +19,7 @@ def selection_sort(array)
     minimum_index = find_minimum_with_index(array)[1]
     sorted_array.push(array.delete_at(minimum_index))    
   end
-return sorted_array  
+  return sorted_array  
 end
 
 print selection_sort([9,8,7,6,5,4,3,2,1]).to_s + "\n"
