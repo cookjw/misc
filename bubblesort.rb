8b561620d7d4aa0c3801dd8099aa749ab5a27f95
James Cook <cookjw@gmail.com>
2014-11-11 19:15:59 -0800

    bubblesort.rb revised

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,44 +1,35 @@
 # Bubble Sort in Ruby
 # (sorts a list into ascending order using "bubble sort" algorithm)
 
-def sort()
-  puts "Please enter the list (of numbers) you would like to sort. (Elements should be separated by commas, e.g. \"1, 2, 3, 4\".)"
-  rawinput = gets.chomp
-  inputlist = rawinput.split(",").map { |s| s.to_f } # list elements converted to floats. Don't know how to accept array (instead of string) as user input, like Python input().
-    
-  def check(list) # checks if list is asscending order; "true" if so, "false" if not
-    condition = true 
-    for n in (0...list.length - 1)
-      if list[n] > list[n+1] then 
-        condition = false
-        break 
-      else
-        next              
-      end          
-    end
-    return condition  
+def bubble(array)
+  for index in (0...array.length - 1)
+    if array[index] > array[index + 1]
+      greater = array[index]
+      lesser = array[index + 1]
+      array[index] = lesser
+      array[index + 1] = greater
+    end  
   end
-  
-  def fix(list) # sorts list into ascending order
-    for n in (0...list.length - 1)
-      if list[n] <= list[n+1] then
-        next
-      else
-        a = list[n]
-        b = list[n+1]
-        list[n] = b
-        list[n+1] = a      
-      end    
-    end
-  end
-
-  while check(inputlist) == false
-    fix(inputlist)
-  end
-  
-  puts inputlist
-  
 end
 
-sort()
+def sorted?(array)  
+  for index in (0...array.length - 1)
+    if array[index] > array[index + 1]
+      return false
+    end    
+  end
+  return true
+end
 
+def bubble_sort(array)
+  until sorted?(array)
+    bubble(array)
+  end
+  return array  
+end
+
+
+print bubble_sort([9,8,7,6,5,4,3,2,1]).to_s + "\n"
+
+
+print bubble_sort([9,8,6,6,5,100,3,2,1]).to_s + "\n"
