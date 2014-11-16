200f1b8b6e78d41bfefde135a7eadba66b061b41
James Cook <cookjw@gmail.com>
2014-11-16 15:55:43 -0800

    mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -18,9 +18,9 @@ def remove_duplicates(list): # Shouldn't this be built in?
 
 def merge(list1, list2):
     new_list = []    
-    while list1 and list2:        
+    while list1 and list2:  
         smaller = min(list1[0], list2[0])
-        new_list.append(smaller)                
+        new_list.append(smaller)        
         if smaller == list1[0]:
             list1.pop(0)
         elif smaller == list2[0]:
@@ -33,8 +33,34 @@ def merge(list1, list2):
         remainder = list2
     else:
         raise Exception("unknown impossible error")
-    new_list += remove_duplicates([min(remainder), max(remainder)])      
-    return new_list            
+    new_list += remainder     
+    return new_list 
+
+# sort by repeated merging
+# - split into lists of length 1
+# - if even number, merge each with adjacent; if odd number, merge each
+# with adjacent up to penultimate, then merge final 1-element list into
+# preceding 2-element list 
+# - keep merging adjacent lists until only one list remains; then
+# return that list, which should be the sorted result
+
+
+def merge_sort(list):
+    lists = [[item] for item in list]
+    while len(lists) > 1:    
+        if len(lists) % 2 == 0:
+            lists = [merge(lists[index], lists[index+1]) for index in
+                        range(0, len(lists) - 1,2)]
+        elif len(lists) % 2 == 1:
+            prelists = [merge(lists[index], lists[index+1]) for index in 
+                        range(0, len(lists) - 2, 2)] 
+            lists = prelists[:-1] + [merge(prelists[-1], lists[-1])]
+    return lists[0]
+            
+            
+        
+        
+        
         
         
 
--- test_mergesort.py
+++ test_mergesort.py
@@ -1,15 +1,19 @@
 import unittest
-from mergesort import merge
+from mergesort import merge, merge_sort
 
 class MergeTest(unittest.TestCase):
     def test_merge(self):
         self.assertEqual(merge([6], [5]), [5,6])
         self.assertEqual(merge([7,8],[2,4]), [2,4,7,8])
-        self.assertEqual(merge([1,3,5,6], [2,4,7,8]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(merge([1,3,5,6], [2,4,7,8]), [1,2,3,4,5,6,7,8])        
+        self.assertEqual(merge([1,2],[3]),[1,2,3])
+        self.assertEqual(merge([6,7,8,9], [1,2,3,4,5]), [1,2,3,4,5,6,7,8,9])       
         
         
-# class MergeSortTest(unittest.TestCase):
-    # def test_merge_sort(self):
+class MergeSortTest(unittest.TestCase):
+    def test_merge_sort(self):
+        self.assertEqual(merge_sort([9,8,7,6,5,4,3,2,1]), [1,2,3,4,5,6,7,8,9])
+        self.assertEqual(merge_sort([9,8,7,6,6,5,23,1]), [1,5,6,6,7,8,9,23])
 
 if __name__ == '__main__':
     unittest.main() 
