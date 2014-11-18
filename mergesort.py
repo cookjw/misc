1bf11621d1f5ea1607b5ddf92d866c19cd397834
James Cook <cookjw@gmail.com>
2014-11-17 18:43:55 -0800

    mergesort.rb

--- mergesort.py
+++ mergesort.py
@@ -38,14 +38,13 @@ def merge(list1, list2):
 # - keep merging adjacent lists until only one list remains; then
 # return that list, which should be the sorted result
 
-# (This is text I'm going to remove, to see if I can successfully remove things.)
 
 def merge_sort(list):
     lists = [[item] for item in list]
     while len(lists) > 1:    
         if len(lists) % 2 == 0:
             lists = [merge(lists[index], lists[index+1]) for index in
-                        range(0, len(lists) - 1,2)]
+                        range(0, len(lists) - 1, 2)]
         elif len(lists) % 2 == 1:
             prelists = [merge(lists[index], lists[index+1]) for index in 
                         range(0, len(lists) - 2, 2)] 
--- mergesort.rb
+++ mergesort.rb
@@ -1,5 +1,14 @@
 # Merge Sort in Ruby
 
+# merge two lists
+# - create new list to hold result of merging
+# - start pointer at first element of first list
+# - compare first element of first list to first element of second
+# - whichever is smaller, put in new list
+# - move pointer (if necessary) to element not moved
+# - repeat until a list is empty; move the remaining element(s) of the 
+# other list (in sorted order) to the new list 
+
 
 def merge(array1, array2)
   new_array = []
@@ -25,5 +34,32 @@ def merge(array1, array2)
   
   new_array += remainder
   return new_array
+
+end
+
+# sort by repeated merging
+# - split into lists of length 1
+# - if even number, merge each with adjacent; if odd number, merge each
+# with adjacent up to penultimate, then merge final 1-element list into
+# preceding 2-element list 
+# - keep merging adjacent lists until only one list remains; then
+# return that list, which should be the sorted result
+
+def merge_sort(array)
+  arrays = array.map{|item| [item]}
+  while arrays.length > 1
+    if arrays.length % 2 == 0
+      arrays = (0...arrays.length - 1).step(2).map do
+        |index| merge(arrays[index], arrays[index+1])
+      end
+    elsif arrays.length % 2 == 1
+      prearrays = (0...arrays.length - 2).step(2).map do
+        |index| merge(arrays[index], arrays[index+1])
+      end
+      arrays = prearrays[0...-1] + [merge(prearrays[-1], arrays[-1])]     
+    end
+  end
+  
+  return arrays[0]
 
 end
