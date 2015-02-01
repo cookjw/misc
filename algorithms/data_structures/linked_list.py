4cb069d149d3446b8b31c39bec32c798ac24ec94
James Cook <cookjw@gmail.com>
2015-02-01 11:41:51 -0800

    changed order of things (in multiple senses)

--- algorithms/data_structures/linked_list.py
+++ algorithms/data_structures/linked_list.py
@@ -13,65 +13,6 @@ class ObjectThatCouldBeInList:
         
 def objectify(x):
     return ObjectThatCouldBeInList(x)
-        
-
-class LinkedListDouble:
-
-    def __init__(self, *args):
-        self.head = None        
-        for item in args:
-            self.insert(item)
-            
-    def search(self, item):
-        x = self.head
-        while (x is not None) and x.value != item:
-            x = x.next
-        return x
-        
-    def insert(self, x): 
-        x = objectify(x)    
-        x.next = self.head
-        if self.head is not None:
-            self.head.prev = x
-        self.head = x
-        x.prev = None
-        
-    def delete(self, x):
-        if x.prev is not None:
-            x.prev.next = x.next            
-        else:
-            self.head = x.next            
-        if x.next is not None:
-            x.next.prev = x.prev
-            
-            
-    def reverse(self):        
-        x = self.head
-        while (x is not None):            
-            next = x.next
-            prev = x.prev            
-            x.next = prev
-            x.prev = next
-            if next is None:
-                self.head = x            
-            x = next
-
-        
-    def minimum(self):
-        return self.head
-        
-    def maximum(self):
-        x = self.head
-        while x.next is not None:
-            x = x.next
-        return x
-        
-    def successor(self, x):
-        return x.next
-        
-    def predecessor(self, x):
-        return x.prev
-        
     
 
 class LinkedListSingle:
@@ -148,7 +89,62 @@ class LinkedListSingle:
                     
         
         
+class LinkedListDouble:
+
+    def __init__(self, *args):
+        self.head = None        
+        for item in args[::-1]:
+            self.insert(item)
+            
+    def search(self, item):
+        x = self.head
+        while (x is not None) and x.value != item:
+            x = x.next
+        return x
         
+    def insert(self, x): 
+        x = objectify(x)    
+        x.next = self.head
+        if self.head is not None:
+            self.head.prev = x
+        self.head = x
+        x.prev = None
+        
+    def delete(self, x):
+        if x.prev is not None:
+            x.prev.next = x.next            
+        else:
+            self.head = x.next            
+        if x.next is not None:
+            x.next.prev = x.prev
+            
+            
+    def reverse(self):        
+        x = self.head
+        while (x is not None):            
+            next = x.next
+            prev = x.prev            
+            x.next = prev
+            x.prev = next
+            if next is None:
+                self.head = x            
+            x = next
+
+        
+    def minimum(self):
+        return self.head
+        
+    def maximum(self):
+        x = self.head
+        while x.next is not None:
+            x = x.next
+        return x
+        
+    def successor(self, x):
+        return x.next
+        
+    def predecessor(self, x):
+        return x.prev        
         
 
     
